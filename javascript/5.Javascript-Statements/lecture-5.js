b0514884c8596413ec00de92068d3dca2700152d
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-20 14:34:43 +0530

    20-03

---
+++ javascript/5.Javascript-Statements/lecture-5.js
@@ -0,0 +1,123 @@
+/* JavaScript String Oprators */
+
+// let String1 = "Hi, I am Javascript!";
+// let String2 = "Hello, Good Afternoon!";
+// let NewString = String1 + String2;
+// console.log(NewString);
+
+// /* typeof Oprators */
+// let a = "SkillQode";
+// let b = 4.6544;
+// let c = false;
+// let d = 2135415346546546546544654654n;
+// let e = Symbol("dataTypes");
+// let f = [];
+// let g = {};
+// let h = function(){};
+// let i = null;
+// let j = undefined
+
+// console.log(typeof(a));
+//console.log(typeof(b));
+//console.log(c);
+//console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(typeof(g));
+// console.log(typeof(h));
+//console.log(typeof(i));
+//console.log(typeof(j));
+
+/* Javascript Statements */
+
+/*
+if Statements
+
+if else Statements
+
+if else if......Statements
+
+Nested Statements
+
+Switch Statements
+*/
+
+// {
+//     let Std = 10;
+//     if (Std < 10) {
+//         console.log("you are not eligible for higher ducation!");
+//     }
+// }
+
+
+/* if else Statements */
+
+// let Std = 9;
+
+// if (Std < 10) {
+//     console.log("you are not eligible for higher Education!");
+// }
+// else {
+//     console.log("eligible for higher Education!");
+// }
+
+
+/* if else if... Statements */
+
+
+// let Std = 12;
+
+// if (Std >= 10) {
+//     console.log("eligible for Diploma Education!");
+// }
+// else if (Std >= 12) {
+//     console.log("eligible for Degree Education!");
+// }
+// else {
+//     console.log("you are not eligible for Degree/Diploma Education!");
+// }
+
+// let Std = 9;
+// if (Std >= 12) {
+//     console.log("eligible for Degree Education!");
+// } else if (Std >= 10) {
+//     console.log("eligible for Diploma Education!");
+// }
+// else {
+//     console.log("you are not eligible for Degree/Diploma Education!");
+// }
+
+
+/* Nested Statements */
+
+// let Std = 10;
+
+// if (Std > 10) {
+//     if (Std > 10) {
+//     }
+//     else {
+//     }
+// }
+// else {
+// }
+
+
+/* Voting CriteArea */
+
+/* Switch Statements */
+
+// let book = "Maths";
+
+// switch (book) {
+//     case "Maths": console.log("Book are Available!");
+//         break;
+//     case "Chemistry": console.log("Book are Available!");
+//         break;
+//     case "Chemistry": console.log("Book are Available!");
+//         break;
+//     case "Physics": console.log("Book are Available!");
+//         break;
+//     case "Biology": console.log("Book are Available!");
+//     default: console.log("Book are Not Available!");
+// }
---
+++ javascript/Project/19-03-24/index.js
@@ -0,0 +1,86 @@
+/* 1. Check if a number is positive, negative, or zero */
+
+// let num = 11;
+
+// if (num > 0) {
+//     console.log("Positive");
+// } else if (num < 0) {
+//     console.log("Negative");
+// } else {
+//     console.log("Zero");
+// }
+
+/* 2. Check if a person is eligible to vote */
+
+// let age = 18;
+// let voterID = "yes";
+
+// if (age >= 18) {
+//     if (voterID >= "yes") {
+//         console.log("You are eligible for vote");
+//     }
+//     else {
+//         console.log("You are not eligible to vote because you have no ID ");
+//     }
+// }
+// else {
+//     console.log("You are not eligible for vote, due age");
+// }
+
+
+/* 3. Determine the largest of three numbers */
+
+// let a = 15;
+// let b = 10;
+// let c = 20;
+
+// if (a >= b) {
+//     if (a >= c)
+//         console.log("A is the largest number.");
+//     else
+//         console.log("C is the largest number.");
+// }
+// else {
+//     if (b >= c)
+//         console.log("B is the largest number.");
+//     else
+//         console.log("C is the largest number.");
+// }
+
+
+/* 4. Check if a year is a leap year */
+
+// let year = 2024;
+
+// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//     console.log("Yes, It is a leap year.");
+// }
+// else {
+//     console.log("No, It is Not a leap year.");
+// }
+
+
+/* 5. Determine the grade based on a score */
+
+// let marks = 50;
+
+// if (marks <= 34) {
+//     console.log("You are fail");
+// }
+// else {
+//     if (marks >= 95) {
+//         console.log("Yot get Grade = A");
+//     }
+//     else if (marks >= 80) {
+//         console.log("Yot get Grade = B");
+//     }
+//     else if (marks >= 60) {
+//         console.log("Yot get Grade = C");
+//     }
+//     else if (marks >= 40) {
+//         console.log("Yot get Grade = D");
+//     }
+//     else if (marks >= 35) {
+//         console.log("Yot get Grade = E");
+//     }
+// }
