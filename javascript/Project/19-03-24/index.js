28ad50544eb75ccf2654d06c47cbbeed80d4ef0c
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-21 14:53:18 +0530

    21-03

--- javascript/Project/19-03-24/index.js
+++ javascript/Project/19-03-24/index.js
@@ -1,86 +1,86 @@
 /* 1. Check if a number is positive, negative, or zero */
 
-// let num = 11;
+let num = 0;
 
-// if (num > 0) {
-//     console.log("Positive");
-// } else if (num < 0) {
-//     console.log("Negative");
-// } else {
-//     console.log("Zero");
-// }
+if (num > 0) {
+    console.log("Positive");
+} else if (num < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
 
 /* 2. Check if a person is eligible to vote */
 
-// let age = 18;
-// let voterID = "yes";
+let age = 18;
+let voterID = "yes";
 
-// if (age >= 18) {
-//     if (voterID >= "yes") {
-//         console.log("You are eligible for vote");
-//     }
-//     else {
-//         console.log("You are not eligible to vote because you have no ID ");
-//     }
-// }
-// else {
-//     console.log("You are not eligible for vote, due age");
-// }
+if (age >= 18) {
+    if (voterID >= "yes") {
+        console.log("You are eligible for vote");
+    }
+    else {
+        console.log("You are not eligible to vote because you have no ID ");
+    }
+}
+else {
+    console.log("You are not eligible for vote, due age");
+}
 
 
 /* 3. Determine the largest of three numbers */
 
-// let a = 15;
-// let b = 10;
-// let c = 20;
+let a = 15;
+let b = 10;
+let c = 20;
 
-// if (a >= b) {
-//     if (a >= c)
-//         console.log("A is the largest number.");
-//     else
-//         console.log("C is the largest number.");
-// }
-// else {
-//     if (b >= c)
-//         console.log("B is the largest number.");
-//     else
-//         console.log("C is the largest number.");
-// }
+if (a >= b) {
+    if (a >= c)
+        console.log("A is the largest number.");
+    else
+        console.log("C is the largest number.");
+}
+else {
+    if (b >= c)
+        console.log("B is the largest number.");
+    else
+        console.log("C is the largest number.");
+}
 
 
 /* 4. Check if a year is a leap year */
 
-// let year = 2024;
+let year = 2024    ;
 
-// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-//     console.log("Yes, It is a leap year.");
-// }
-// else {
-//     console.log("No, It is Not a leap year.");
-// }
+if (year % 4 == 0) {
+    console.log("Yes, It is a leap year.");
+}
+else {
+    console.log("No, It is Not a leap year.");
+}
 
 
 /* 5. Determine the grade based on a score */
 
-// let marks = 50;
+let marks = 50;
 
-// if (marks <= 34) {
-//     console.log("You are fail");
-// }
-// else {
-//     if (marks >= 95) {
-//         console.log("Yot get Grade = A");
-//     }
-//     else if (marks >= 80) {
-//         console.log("Yot get Grade = B");
-//     }
-//     else if (marks >= 60) {
-//         console.log("Yot get Grade = C");
-//     }
-//     else if (marks >= 40) {
-//         console.log("Yot get Grade = D");
-//     }
-//     else if (marks >= 35) {
-//         console.log("Yot get Grade = E");
-//     }
-// }
+if (marks <= 34) {
+    console.log("You are fail");
+}
+else {
+    if (marks >= 95) {
+        console.log("Yot get Grade = A");
+    }
+    else if (marks >= 80) {
+        console.log("Yot get Grade = B");
+    }
+    else if (marks >= 60) {
+        console.log("Yot get Grade = C");
+    }
+    else if (marks >= 40) {
+        console.log("Yot get Grade = D");
+    }
+    else if (marks >= 35) {
+        console.log("Yot get Grade = E");
+    }
+}
