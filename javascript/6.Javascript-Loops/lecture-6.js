475b6ac106d6fdaf754dbb297cdc0af3d543a3ae
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-22 15:56:01 +0530

    22-03

---
+++ javascript/6.Javascript-Loops/lecture-6.js
@@ -0,0 +1,104 @@
+/* Javascript Template Literals */
+
+// let String1 = "Hi, I 'Am' A Frontend Developer!";
+
+// let String2 = 'Hi, I "Am" A Frontend Developer!';
+
+// let String3 = "'Hi, I Am A Frontend Developer!'"; 
+
+// let String4 = "Hi, I \"Am\" A Frontend Developer!";
+
+// let String5 = 'Hi, I \'Am\' A Frontend Developer!'
+
+// console.log(String1);
+// console.log(String2); 
+// console.log(String3);
+// console.log(String4); 
+// console.log(String5);
+
+// let String6 = `Hi, I 'Am' A 
+// Frontend Developer!`;
+
+// console.log(String);
+
+/* Passing The Variables */
+
+// let text = "Developer";
+
+// let String7="Hi, I 'Am' A Frontend ${text}!"; 
+// let String8 = `Hi, I 'Am' A Frontend ${text}!`;
+
+//console.log(String7);
+// console.log(String8);
+
+/* Javascript Loops */
+
+/*
+while loop do-while loop
+for loop
+for in... loop
+for of... loop
+*/
+
+/* While Loop */
+
+/*
+while (condition) {
+    statements
+}
+*/
+
+// let Num = 0;
+
+// while (Num <= 10){
+// console.log("Hello Javascript!");
+// document.write("Hello Javascript!" + "<br>");
+// document.write(`Hello Javascript! ${Num}` + "<br>"); 
+// document.getElementById("demo").innerHTML = "Hello World!" + Num;
+// Num++;
+// }
+
+
+/* do-while loop */
+
+/*
+do{
+Statements;
+}while(condition);
+*/
+
+// let i = -10;
+
+// do{
+// document.write(`Our Loop! ${i}` + "<br>");
+// i++;
+// }while(i <= 10)
+
+
+/* For Loop */
+
+/*
+for (initilization; condition; afterthought) {
+}
+*/
+
+// let i = 0;
+// let n = 10;
+
+// for (i; i < n; i++) {
+//     console.log("Jay Mataji!");
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log("Hello!");
+// }
+// for (i = 10; i < 10; i++) {
+//     console.log("Hello!");
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Hello!");
+// }
+// for (const i = 0; i < 10; i++) {
+//     console.log("Hello!");
+// }
