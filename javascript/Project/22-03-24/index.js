b6760da06d4159a5357e764d1b61a2ecefb63d30
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-26 09:21:29 +0530

    26-03-24

---
+++ javascript/Project/22-03-24/index.js
@@ -0,0 +1,74 @@
+/* 1. Print the Numbers from 0 to 15. */
+
+// for (let i = 0; i < 16; i++) {
+//     console.log(i);
+// }
+
+
+/* 2. Print the Numbers from 12 to 24. */
+
+// for (let j = 12; j < 25; j++) {
+//     console.log(j);
+// }
+
+
+
+/* 3.  Print the ODD Numbers from 7 to 31. */
+
+// let m = 0;
+
+// for (let n = 7; n <= 31; n += 2) {
+//     n = n + m;
+//     console.log(n);
+// }
+
+
+/* 4. Print the EVEN Numbers from 10 to -20. */
+// let a = 0;
+// for (a = -20; a < 10; a++) {
+//     if (a % 2 === 0) {
+//         console.log(a);
+//     }
+// }
+
+
+/* 5. Iterate through all numbers from 1 to 45. Print the
+following:
+● For multiples of 3 print “Fizz”
+● For multiples of 5 print “Buzz”
+● For multiples of 3 and 5 print “FizzBuzz” */
+
+
+
+
+
+
+
+
+
+
+/* 6. Print all the elements of the following array
+var thisIsAnArray = ["element1", "element2", "element3",
+"element4"]; */
+
+
+// let thisIsAnArray = ["element1", "element2", "element3", "element4"];
+
+// for (let key in thisIsAnArray) {
+//         console.log(`${thisIsAnArray[key]}`);
+//     }
+
+
+/* 7. Calculate the sum of all the numbers in the following array
+      var numbersArray = [1,13,22,123,49] */
+
+// let numbersArray = [1, 13, 22, 123, 49];
+// let sum = 0;
+
+// for (let i = 0; i < numbersArray.length; i++) {
+//     sum = sum + numbersArray[i];
+// }
+// for (let key in numbersArray) {
+//     console.log(`${numbersArray[key]}`);
+// }
+// console.log("Total is:" + sum);
