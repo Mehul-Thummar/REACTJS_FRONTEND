5ae041ff10a9ec654d620aaa5609380bd1c05e63
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-26 16:01:11 +0530

    26-03

---
+++ javascript/8.Map_And_Set_Method/lecture-8.js
@@ -0,0 +1,213 @@
+/* Javascript Map Object Method */
+
+/*
+new Map()
+set Method get Method
+delete Method
+clear Method
+key Method
+has Method
+forEach Method
+Values Method
+size Method
+entries Method
+*/
+
+// let Object = { id: 1, FirstName: "joe"LastName: "Hilson"Age: 55 };
+
+// let DataObject = Object.Age
+
+// console.log(DataObject);
+
+
+/* Map Object */
+
+// let MapObject = new Map();
+
+/* Set Method */
+
+// MapObject.set("id", 1)
+// MapObject.set("FirstName", "Joe")
+// MapObject.set("LastName", "Hilson")
+// MapObject.set("Age", 55)
+
+// console.log(MapObject);
+
+
+/* get Method */
+
+// let DataObject1 = MapObject.get("LastName")
+// let DataObject2 = MapObject.get("Age")
+
+// console.log(DataObject1);
+// console.log(DataObject2);
+
+
+/* Delete Method */
+
+// let DeleteMap = MapObject.delete("Age")
+
+// console.log(MapObject);
+
+// console.log(DeleteMap);
+
+
+/* size Method */
+// let SizeMap = MapObject .size;
+
+// console.log(SizeMap);
+
+
+/* Values Method */
+
+// let ValueMap = MapObject.values();
+
+// console.log(ValueMap);
+// console.log(ValueMap.next().value);
+// console.log(ValueMap.next().value);
+// console.log(ValueMap.next().value);
+
+
+/* Key Method */
+
+// let MapObject = new Map([["id", 1], ["FirstName", "Joe"], ["LastName", "Hilson"], ["Age", 55]])
+
+// let KeyData = MapObject.keys();
+
+// console.log(KeyData);
+// console.log(KeyData.next().value);
+// console.log(KeyData.next().value);
+// console.log(KeyData.next().value);
+// console.log(KeyData.next().value);
+
+
+/* Has Method */
+
+// let MapObject = new Map([["id" = new Map([["id", 1], ["FirstName", "Joe"], ["LastName", "Hilson"], ["Age", 55]])
+
+// let HasMap = MapObject.has ("FirstName")
+
+//console.log(HasMap);
+
+
+/* clear() Method */
+
+// let MapObject = new Map([["id" = new Map([["id", 1], ["FirstName", "Joe"], ["LastName", "Hilson"], ["Age", 55]])
+
+// let ClearMap = MapObject.clear();
+
+// console.log(MapObject);
+
+
+/* forEach Method */
+
+// let MapObject = new Map([["id", 1], ["FirstName", "Hilson"], ["LastName", "Joe"], ["Age", 55]]).forEach(DataPrint)
+
+
+/* Set Method */
+
+// MapObject.set("id", 1)
+// MapObject.set("FirstName", "Joe")
+// MapObject.set("LastName", "Hilson")
+// MapObject.set("Age", 55)
+
+// console.log(MapObject);
+
+// function DataPrint(value, key) {
+//     console.log(`${key} = ${value}`);
+// }
+
+
+/* entries () Method */
+
+// let MapObject = new Map([["id", 1], ["FirstName", "Joe"], ["LastName", "Hilson"], ["Age", 55]])
+
+// let Entries = MapObject.entries()
+// console.log(Entries);
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+
+
+/* Javascript Set Object */
+
+/* add Method */
+
+// let Setoject = new Set();
+
+// let AddSets = Setoject.add("Apple")
+// let AddSets1 = Setoject.add("Graps")
+// let AddSets2 = Setoject.add("Mango")
+// let AddSets3 = Setoject.add("Strawbarry")
+
+
+/* entries Method */
+
+// let Entries = Setoject.entries()
+
+// console.log(Entries);
+
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+// console.log(Entries.next().value);
+
+//console.log(AddSets);
+
+
+/* has Method */
+
+// let GetSets = Setoject.has("Apple")
+// console.log(GetSets);
+
+
+/* Key Method */
+
+// let KeySets = Setoject.keys();
+
+// console.log(KeySets);
+
+// console.log(KeySets.next().value);
+// console.log(KeySets.next().value);
+// console.log(KeySets.next().value);
+// console.log(KeySets.next().value);
+
+
+/* Values Method */
+
+// let ValueSets = Setoject.values();
+
+// console.log(ValueSets);
+// console.log(ValueSets.next().value);
+// console.log(ValueSets.next().value);
+// console.log(ValueSets.next().value);
+// console.log(ValueSets.next().value);
+
+
+/* size Method */
+
+// let SizeSets = Setoject.size
+
+// console.log(SizeSets);
+
+
+/* Delete Method */
+
+// let DeleteSets = Setoject.delete("Apple")
+// console.log(Setoject);
+
+
+/* clear Method */
+
+// let ClearSets = Setoject.clear()
+// console.log(Setoject);
+
+
+/* forEach Method */
+
+// let ForEachSets = Setoject.forEach(PrintSets)
+// function PrintSets(value, key) {
+
+//     console.log(`${key} = ${value}`);
+// }
