ecf67ad12748bbbadbacd860bf13f03d7c51cba8
Mehul-Thummar <bbsofcbackup@gmail.com>
2024-03-30 15:17:02 +0530

    30-03

---
+++ javascript/10.Date_Object/lecture-10.js
@@ -0,0 +1,84 @@
+/* Javascript Number Method */
+
+/* isNaN()*/
+
+// let num = NaN;
+
+// let Num = Number.isNaN(num)
+
+// console.log(Num);
+
+
+/* parseFloat, parseintiger() */
+
+// let num = 10.10545664654;
+
+// let Num1 = Number.parseFloat(num)
+
+// console.log(Num1);
+
+// let Num2 = Number.parseInt(num)
+
+// console.log(Num2);
+
+
+/* Date Method in Javascript */
+
+// let NewDate = new Date('28 March 2024 05:15:15 GMT');
+
+// console.log(NewDate);
+
+
+/* Get Method In Date Object */
+
+// let NewDate1 = NewDate.getDate()
+
+// console.log(NewDate1);
+
+// console.log(NewDate.getDay());
+
+// console.log(NewDate.getFullYear());
+
+// console.log(NewDate.getHours());
+
+// console.log(NewDate.getMinutes());
+
+// console.log(NewDate.getSeconds());
+
+// console.log(NewDate.getMonth());
+
+
+/* date.parse() method */
+
+// let Dates = Date.parse(new Date())
+
+// console.log(Dates);
+
+
+/* Set Method in Date Object */
+
+// let Dates = new Date()
+
+// console.log(Dates);
+
+// let Dates1 = Dates.setDate(15)
+
+// console.log(Dates1);
+
+// console.log(Dates);
+
+// let Dates2 = Dates.setFullYear(2000)
+
+// console.log(Dates);
+
+// let Dates3 = Dates.setHours(24)
+
+// console.log(Dates);
+
+// let Dates4 = Dates.setMinutes(30)
+
+// console.log(Dates);
+
+// let Dates5 = Dates.setSeconds(60)
+
+// console.log(Dates);
